f22ff30f07b12bae57b723373fc1a882234e8332
youbaidjis <youba.etud@gmail.com>
2022-09-05 23:09:14 -0700

    first commit

---
+++ lib/algerography.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "algerography/version"
+
+module Algerography
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/algerography/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Algerography
+  VERSION = "0.1.0"
+end
